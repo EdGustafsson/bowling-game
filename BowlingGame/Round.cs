afb098d3481166068eab4beb79eb757460c4c888
EdGustafsson <edlimmet@gmail.com>
2020-11-29 13:26:35 +0100

    Start

---
+++ BowlingGame/Round.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingGame
+{
+    public class Round
+    {
+        static void Roll(int pins)
+        {
+           
+        }
+
+        static void Score()
+        {
+           
+        }
+    }
+}
