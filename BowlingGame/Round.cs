bfa9c699c37037ebb445a5d1607c467078cd4ab2
EdGustafsson <edlimmet@gmail.com>
2020-11-30 07:55:19 +0100

    Round class

--- BowlingGame/Round.cs
+++ BowlingGame/Round.cs
@@ -6,16 +6,42 @@ namespace BowlingGame
 {
     public class Round
     {
+        int[] rolls = new int[21];
+        int currentRoll;
 
-        int score;
         public void Roll(int pins)
         {
-            score += pins;
+
+            rolls[currentRoll] = pins;
+            currentRoll++;
         }
 
         public int Score()
         {
+            int roll = 0;
+            int score = 0;
+        
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if (rolls[roll] == 10)
+                {
+                    score += 10 + rolls[roll + 1] + rolls[roll + 2];
+                    roll++;
+                }
+                if (rolls[roll] + rolls[roll+1] == 10)
+                {
+                    score += 10 + rolls[roll + 2];
+                    roll += 2;
+                }
+                else
+                {
+                    score += rolls[roll] + rolls[roll + 1];
+                    roll += 2;
+                }
+            }
+
             return score;
+
         }
     }
 }
--- BowlingGameTest/RoundTests.cs
+++ BowlingGameTest/RoundTests.cs
@@ -29,5 +29,71 @@ namespace BowlingGameTest
 
             Assert.AreEqual(a.Score(), 0);
         }
+
+        [TestMethod]
+        public void Score_RollSpare_ReturnsCorrectScore()
+        {
+            Round a = new Round();
+
+            a.Roll(5);
+            a.Roll(5);
+            a.Roll(1);
+
+            for (int i = 0; i > 17; i++)
+            {
+                a.Roll(0);
+            }
+
+            Assert.AreEqual(a.Score(), 12);
+
+        }
+
+        [TestMethod]
+        public void Score_RollStrike_ReturnsCorrectScore()
+        {
+            Round a = new Round();
+
+            a.Roll(10); 
+            a.Roll(2);
+            a.Roll(2);  // 14 + 4
+
+            for (int i = 0; i > 17; i++)
+            {
+                a.Roll(0);
+            }
+
+            Assert.AreEqual(a.Score(), 18);
+
+        }
+
+        [TestMethod]
+        public void TestScore()
+        {
+            Round a = new Round();
+
+            a.Roll(10);// Strike 14
+            a.Roll(2);//  16
+            a.Roll(2);//  18
+            a.Roll(0);//  18
+            a.Roll(9);//  27
+            a.Roll(4);//  
+            a.Roll(6);//  39
+            a.Roll(2);//  41
+            a.Roll(2);//  43
+            a.Roll(0);//  43
+            a.Roll(10);// Spare 55
+            a.Roll(2);//  57 
+            a.Roll(8);//  Spare 67
+            a.Roll(2);//  69
+            a.Roll(2);//  71
+            a.Roll(0);//
+            a.Roll(0);//
+            a.Roll(0);//
+            a.Roll(0);//
+
+
+            Assert.AreEqual(a.Score(), 71);
+
+        }
     }
 }
